b1a82160a3cc97aa6e6d453df6d6f11a4af84cb3
Coppertine <37494321+Coppertine@users.noreply.github.com>
2020-04-02 22:05:32 +1000

    Create UpdaterType.cs

---
+++ Tsutsuji.Updater/Screens/UpdaterType.cs
@@ -0,0 +1,11 @@
+namespace Tsutsuji.Updater.Screens
+{
+  public enum UpdaterType
+  {
+    /// <summary> Applies the updater and installer if used for the first time. </summary>
+    First,
+  
+    /// <summary> Only applies the updater as if the user has used it more than once. </summary>
+    Update
+  }
+}
